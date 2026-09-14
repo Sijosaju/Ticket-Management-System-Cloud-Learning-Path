739d71cd4dfda732d8bf9006782679fe530f3ce5
Sijo <sijosaju15@gmail.com>
2026-09-14 21:31:20 +0530

    all commit

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,4 +1,4 @@
-const API = 'http://localhost:5000/api';
+const API = window.APP_CONFIG.API_URL;
 const token = () => localStorage.getItem('token');
 const user  = () => JSON.parse(localStorage.getItem('user') || 'null');
 
